1d7f0ea102e6cf134f230122dbcd62e61275ce1f
Leonard Lobel <lenni.lobel@sleektech.com>
2024-08-26 17:46:22 -0400

    Sync to public

--- Rag/Scripts/Rag.TestScripts.sql
+++ Rag/Scripts/Rag.TestScripts.sql
@@ -8,8 +8,8 @@ SELECT COUNT(*) / 1536 FROM [rag-demo-ada].dbo.MovieVector
 SELECT COUNT(*) FROM Movie WHERE Vectors IS NOT NULL	-- EAP
 SELECT COUNT(*) / 3072 FROM MovieVector					-- 3072 dimensions (3l)
 SELECT COUNT(*) / 1536 FROM MovieVector					-- 1536 dimensions (3s, ada)
-DECLARE @Endpoint varchar(max) = 'https://lenni-openai.openai.azure.com/'
-DECLARE @ApiKey varchar(max) = '1e981882b329481ebe4b2bfa261f8dce'
+DECLARE @Endpoint varchar(max) = '[ENDPOINT]'
+DECLARE @ApiKey varchar(max) = '[API-KEY]'
 DECLARE @ModelName varchar(max)
 SET @ModelName = 'lenni-text-embedding-3-large'		-- 3l
 SET @ModelName = 'lenni-text-embedding-3-small'		-- 3s
